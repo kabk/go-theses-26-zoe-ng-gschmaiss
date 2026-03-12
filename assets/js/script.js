13dbc130da614c709e7d99295430946b2b51e695
Zoezeryinn <zoezeryinn@gmail.com>
2026-03-12 12:59:04 +0100

    change

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+
+
+// we make sure the JavaScript file loads after our HTML by using a function test if the HTML is loaded
+
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}   
+
+
+
+docReady(function() {
+
+	// functions
+	// go
+	// here
+
+});
+
+
